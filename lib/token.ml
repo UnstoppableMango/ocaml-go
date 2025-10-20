a431838f53bd3ff17f32256387c7edec62fb1fc5
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-10-19 19:50:20 -0500

    WIP

--- lib/token.ml
+++ lib/token.ml
@@ -77,31 +77,6 @@ let tokens =
 
 let name t = tokens |> IntMap.find_opt t |> Option.value ~default:"ILLEGAL"
 
-(* TODO *)
-(* let tokens = function
-  | Illegal -> "ILLEGAL"
-  | Eof -> "EOF"
-  | Comment -> "COMMENT"
-  (* Literals *)
-  | Ident -> "IDENT"
-  | Int -> "INT"
-  | Float -> "FLOAT"
-  | Imaginary -> "IMAG"
-  | Char -> "CHAR"
-  | String -> "STRING"
-  (* Operators *)
-  | Add -> "+"
-  | Sub -> "-"
-  | Mul -> "*"
-  | Quo -> "/"
-  | Rem -> "%"
-  (* Keywords *)
-  | Break -> "break"
-  | Case -> "case"
-  | Chan -> "chan"
-  | Const -> "const"
-  | Continue -> "continue" *)
-
 module P : sig
   val is_literal : token -> bool
   val is_operator : token -> bool
@@ -109,16 +84,9 @@ module P : sig
   val is_exported : string -> bool
   (* val is_identifier : string -> bool *)
 end = struct
-  let is_literal = function
-    | literal_begin .. literal_end -> true
-    | _ -> false
-
-  let is_operator = function Add | Sub | Mul | Quo | Rem -> true | _ -> false
-
-  (* TODO *)
-  let is_keyword = function
-    | Break | Case | Chan | Const | Continue -> true
-    | _ -> false
+  let is_literal t = literal_begin < t && t < literal_end
+  let is_operator t = operator_begin < t && t < operator_end
+  let is_keyword t = keyword_begin < t && t < keyword_end
 
   (* TODO: 5.4 Ascii.is_upper *)
   let is_upper = function 'A' .. 'Z' -> true | _ -> false
